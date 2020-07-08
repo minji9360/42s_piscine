45dd1cc5b7f5cf1fb47de26e1a69b480fc69486e
minji9360 <minji4853@naver.com>
2020-07-09 05:07:01 +0900

    Add ex05

---
+++ ex05/ft_str_is_uppercase.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_uppercase.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minjikim <minjikim@student.42seoul.>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/09 02:56:39 by minjikim          #+#    #+#             */
+/*   Updated: 2020/07/09 03:02:09 by minjikim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int ft_str_is_uppercase(char *str);
+
+int main(void)
+{
+	char str[200] = {" "};
+	printf("%d", ft_str_is_uppercase(str));
+}
+
+int ft_str_is_uppercase(char *str)
+{
+	int i;
+	int a;
+
+	i = 0;
+	a = -1;
+	while(str[i] != '\0')
+	{
+		if(!(str[i] >= 'A' && str[i] <= 'Z'))
+		{
+			a = 0;
+			break;
+		}
+		else
+			a = 1;
+		i++;
+	}
+	return a;
+}
