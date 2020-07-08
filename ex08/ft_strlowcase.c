6cf93eb30600f728059deef84f4c1a9b4c2aaba5
minji9360 <minji4853@naver.com>
2020-07-09 05:32:50 +0900

    Add ex08

---
+++ ex08/ft_strlowcase.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlowcase.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minjikim <minjikim@student.42seoul.>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/09 05:08:29 by minjikim          #+#    #+#             */
+/*   Updated: 2020/07/09 05:32:25 by minjikim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char *ft_strlowcase(char *str);
+
+int		main(void)
+{
+	char test[50] = {"sdfSGeDefsD"};
+	char result[50] = {};
+	*result = *ft_strlowcase(test);
+}
+
+char	*ft_strlowcase(char *str)
+{
+	int i;
+	
+	i = 0;
+	while(str[i] != '\0')
+	{
+		if(str[i] >= 'A' && str[i] <= 'Z')
+			str[i] += 32;
+		printf("%c", str[i]);
+		i++;
+	}
+	return str;
+}
