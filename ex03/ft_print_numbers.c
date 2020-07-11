3a0096775512f3ccb7b46983d1e55af06fab5ff3
minji9360 <minji4853@naver.com>
2020-07-11 21:31:41 +0900

    Modify ex03

--- ex03/ft_print_numbers.c
+++ ex03/ft_print_numbers.c
@@ -6,18 +6,11 @@
 /*   By: minjikim <minjikim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/06 02:38:37 by minjikim          #+#    #+#             */
-/*   Updated: 2020/07/06 04:00:23 by minjikim         ###   ########.fr       */
+/*   Updated: 2020/07/11 21:16:15 by minjikim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-
-void ft_print_numbers(void);
-
-int		main(void)
-{
-	ft_print_numbers();
-}
 
 void	ft_print_numbers(void)
 {
