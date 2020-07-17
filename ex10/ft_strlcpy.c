649040459cefbf41aca59f704fd3d1f93bbb35ac
minji9360 <minji4853@naver.com>
2020-07-17 22:45:44 +0900

    Modify ex10

--- ex10/ft_strlcpy.c
+++ ex10/ft_strlcpy.c
@@ -6,49 +6,28 @@
 /*   By: minjikim <minjikim@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/14 18:27:28 by minjikim          #+#    #+#             */
-/*   Updated: 2020/07/15 16:39:15 by minjikim         ###   ########.fr       */
+/*   Updated: 2020/07/17 22:43:17 by minjikim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <string.h>
-
-unsigned int ft_strlcpy(char *dest, char *src, unsigned int size);
-
-int				main(void)
-{
-	char o[20];
-	printf("returned - %lu, string - %s \n", strlcpy(o, "abcedf", 10L), o);
-	printf("returned - %lu, string - %s \n", strlcpy(o, "123456", 6L), o);
-	printf("returned - %lu, string - %s \n", strlcpy(o, "abcedf", 7L), o);
-	printf("returned - %lu, string - %s \n", strlcpy(o, "123456", 3L), o);
-	printf("returned - %lu, string - %s \n", strlcpy(o, "abcef", 0L), o);
-	printf("--------\n");
-	
-	printf("returned - %u, string - %s \n", ft_strlcpy(o, "abcdef", 10L), o);
-	printf("returned - %u, string - %s \n", ft_strlcpy(o, "123456", 6L), o);
-	printf("returned - %u, string - %s \n", ft_strlcpy(o, "abcdef", 7L), o);
-	printf("returned - %u, string - %s \n", ft_strlcpy(o, "123456", 3L), o);
-	printf("returned - %u, string - %s \n", ft_strlcpy(o, "abcef", 0L), o);
-	
-	return (0);
-}
-
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 {
-	int	count;
-	int	int_size;
+	unsigned int	a;
+	unsigned int	b;
 
-	count = 0;
-	int_size = (int)size;
-	while (src[count] != '\0' && count < (int_size - 1))
+	a = 0;
+	b = 0;
+	while (src[a] != '\0')
+		a++;
+	if (size != 0)
 	{
-		dest[count] = src[count];
-		count++;
+		while (src[b] != '\0' && b < (size - 1))
+		{
+			dest[b] = src[b];
+			b++;
+		}
+		dest[b] = '\0';
 	}
-	dest[count] = '\0';
-	while (src[count] != '\0')
-		count++;
 
-	return (count);
+	return (a);
 }
