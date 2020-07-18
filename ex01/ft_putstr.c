890c8b1503671c65b032a464496532cbea59374e
minji9360 <minji4853@naver.com>
2020-07-19 05:05:00 +0900

    Add ex01

---
+++ ex01/ft_putstr.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minjikim <minjikim@student.42seoul.>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/19 04:58:58 by minjikim          #+#    #+#             */
+/*   Updated: 2020/07/19 05:03:53 by minjikim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	char i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	write(1, str, i);
+}
+
+int		main(void)
+{
+	ft_putstr("How Can I alive in Piscine");
+}
