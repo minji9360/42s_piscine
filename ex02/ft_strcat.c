178bb45ae1e44a73134ead6015b59d91398ef2de
minji9360 <minji4853@naver.com>
2020-07-19 04:01:37 +0900

    Modify ex02

--- ex02/ft_strcat.c
+++ ex02/ft_strcat.c
@@ -6,10 +6,38 @@
 /*   By: minjikim <minjikim@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/09 16:55:05 by minjikim          #+#    #+#             */
-/*   Updated: 2020/07/09 16:56:34 by minjikim         ###   ########.fr       */
+/*   Updated: 2020/07/18 19:03:05 by minjikim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
+char *ft_strcat(char *dest, char *src);
 
+int		main(void)
+{
+	char s1[100] = "sdfdDDD";
+	char s2[100] = "1231111";
+
+	printf("%s\n", s1);
+	ft_strcat(s1, s2);
+	printf("%s", s1);
+}
+
+char	*ft_strcat(char *dest, char *src)
+{
+	int count;
+	int i;
+
+	count = 0;
+	i = 0;
+	while (dest[count] != '\0')
+		count++;
+	while (src[i] != '\0')
+	{
+		dest[count + i] = src[i];
+		i++;
+	}
+	dest[count + i] = '\0';
+	return (dest);
+}
