8d62c6bfae3f2289d5920f5a2b70e1a130981e27
minji9360 <minji4853@naver.com>
2020-07-19 04:01:53 +0900

    Modify ex03

--- ex03/ft_strncat.c
+++ ex03/ft_strncat.c
@@ -6,7 +6,7 @@
 /*   By: minjikim <minjikim@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/09 20:33:48 by minjikim          #+#    #+#             */
-/*   Updated: 2020/07/09 20:55:54 by minjikim         ###   ########.fr       */
+/*   Updated: 2020/07/19 03:51:16 by minjikim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,32 @@
 
 char *ft_strncat(char *dest, char *src, unsigned int nb);
 
-int main(void)
+int		main(void)
 {
 	char dest[20];
 	char src[20];
 	strcpy(dest, "To be ");
 	strcpy(src, "or not to be");
-	strncat(dest, src, 6);
+	strncat(dest, src, 3);
 	puts(dest);
 	return 0;
 }
 
-char *ft_strncat(char *dest, char *src, unsigned int nb)
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
 {
-	char *start = dest;
-	while(*dest++)
-		;
-	dest--;
-	while (nb--) {
-		if (!(*dest++ = *src)) 
-			return start;
+	unsigned int	size;
+	unsigned int	i;
+
+	size = 0;
+	i = 0;
+	while (dest[size] != '\0')
+		size++;
+	while (src[i] != '\0' && (i < nb))
+	{
+		dest[size] = src[i];
+		size++;
+		i++;
 	}
-	*dest = '\0';
-	return start;
-}	
+	dest[size] = '\0';
+	return (dest);
+}
