b1243ca60aa3be0df2a912e356654a0af6ad22a7
minji9360 <minji4853@naver.com>
2020-07-20 17:30:58 +0900

    Add ex01

---
+++ ex01/ft_recursive_factorial.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minjikim <minjikim@student.42seoul.>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/20 16:23:39 by minjikim          #+#    #+#             */
+/*   Updated: 2020/07/20 16:51:42 by minjikim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_recursive_factorial(int nb)
+{
+	if (nb < 0)
+		return (0);
+	else if (nb == 0)
+		return (1);
+	else
+		return (nb * ft_recursive_factorial(nb - 1));
+}
